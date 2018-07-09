244372343402a8a4100bd35c3a455978aa00a605
CODREDA <40890109+CODREDA@users.noreply.github.com>
2018-07-09 13:15:48 +0000

    Add files via upload

---
+++ aaREDA.py
@@ -0,0 +1,7 @@
+def massar():
+    m =eval(input("please enter your midterm grade"))
+    f = 160 - m
+    if f<100 :
+        print("you need :",f)
+    else:
+        print("sorry you will have to retake the class")
